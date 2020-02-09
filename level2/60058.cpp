a12669d51b1e0ff8fc8668df367ce9a6ab54d5b3
9yuna <9yuna.b@gmail.com>
2020-02-09 22:09:16 +0900

    add 60058

---
+++ level2/60058.cpp
@@ -0,0 +1,60 @@
+/* 60058번 괄호 변환 */
+
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+bool correct(string s){
+    int sum = 0;
+    for(int i = 0; i < s.length(); i++){
+        if(s.at(i) == '(')
+            sum++;
+        else if(s.at(i) == ')')
+            sum--;
+        if(sum < 0)
+            return false;
+    }
+    return true;
+}
+
+string func(string s) {
+	if (s == "")
+		return "";
+	string u; string v;
+	int i = 0;
+	int left = 0; int right = 0;
+	for (i = 0; i < s.length(); i++) {
+		if (s.at(i) == '(')
+			left++;
+		else if (s.at(i) == ')')
+			right++;
+		if (left == right) {
+			u = s.substr(0, i + 1);
+			v = s.substr(i + 1);
+			break;
+		}
+	}
+	if (correct(u))
+		return u + func(v);
+	else {
+		string temp = "(" + func(v) + ")";
+		u = u.substr(1, u.length() - 2);
+		for (int i = 0; i < u.length(); i++) {
+			if (u.at(i) == '(')
+				temp = temp + ')';
+			else
+				temp = temp + '(';
+		}
+		return temp;
+	}
+}
+
+string solution(string p) {
+	return func(p);
+}
+
+int main(){
+    cout << solution("))((()")<<"\n";
+    return 0;
+}
